9673fbc8a8eab6c6028e0a7b5ec2fc6d7c2e7d47
exosite-RobinZhang <robinzhang@exosite.com>
2019-05-13 19:24:06 +0800

    Add second line lyrics (Version:1.0.3)

--- js/content-script.js
+++ js/content-script.js
@@ -1,27 +1,39 @@
 function updateCCView(data){
-  $("#ccView").html(data);  
+  $("#ccViewFL").html(data);
 }
 
 function show(){
-  var data="";
-  var div=document.createElement("div");
-  document.body.appendChild(div); 
-  div.align="right";
-  div.id="ccView";
-  div.style="font-size:20px;background-color:#000000;z-index:3000;position:fixed;top:0;right:0";
-  div.innerText =data;
-  div.style.color= "#FFFFFF";
+  var dataFL="";
+  var divFL=document.createElement("div");
+  document.body.appendChild(divFL);
+  divFL.align="right";
+  divFL.id="ccViewFL";
+  divFL.style="font-size:20px;background-color:#000000;z-index:3000;position:fixed;top:0;right:0";
+  divFL.innerText =dataFL;
+  divFL.style.color= "#FFFFFF";
+  var dataSL="";
+  var divSL=document.createElement("div");
+  document.body.appendChild(divSL);
+  divSL.align="right";
+  divSL.id="ccViewSL";
+  divSL.style="font-size:20px;background-color:#000000;z-index:3000;position:fixed;top:25px;right:0";
+  divSL.innerText =dataSL;
+  divSL.style.color= "#FFFFFF";
   }
 
 function getLyrics(){
-  var data=$(".ytp-caption-segment").html();
-  return data;
+  var dataFL=$(".ytp-caption-segment").eq(0).html();
+  if ($(".ytp-caption-segment").length >1){
+    var dataSL=$(".ytp-caption-segment").eq(1).html();
+  }else{
+
   }
-  
+  console.log("I get cc lyrics first line>>"+dataFL);
+  console.log("I get cc lyrics second line>>"+dataSL);
+  return dataFL,dataSL;
+  }
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  console.log(message);
-  console.log(sender);
   if (message.action == "createDiv"){
     show();
     sendResponse({ content: "content script finish(createDiv)" });
@@ -32,8 +44,8 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     sendResponse({ content: "content script finish(UpdateDiv)" });
   }
   if (message.action == "getLyrics"){
-    data = getLyrics();
-    sendResponse({ content: "content script finish(getLyrics)" ,ccMessage:data});
+    dataFL ,dataSL= getLyrics();
+    sendResponse({ content: "content script finish(getLyrics)" ,ccMessage:{dataFL:dataFL,dataSL:dataSL}});
   }
 
 });
