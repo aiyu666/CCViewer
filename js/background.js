48eee5ffa6f768ae82d79e354d91260d12ba45f1
RobinZhang <robinzhang@RobinZhangde-MacBook-Pro.local>
2019-12-05 22:12:35 +0800

    Remove no need code

--- js/background.js
+++ js/background.js
@@ -1,38 +1,66 @@
-function backgroundWoker(listenTabId,tabId){
-    chrome.tabs.sendMessage(parseInt(listenTabId),{action:"getLyrics"}, function(response) {
-        var ccMessageFL=response.ccMessage.dataFL;
-        var ccMessageSL=response.ccMessage.dataSL;
-        if (ccMessageFL==undefined){
-            ccMessageFL=null
-        }
-        if (ccMessageSL==undefined){
-            ccMessageSL=null
-        }
-        if (localStorage.getItem(tabId.toString()+"ccMessageFL") != ccMessageFL){
-            chrome.tabs.sendMessage(parseInt(tabId),{action:"UpdateDiv",id:"ccViewFL",ccMessage:ccMessageFL}, function(res) {
-                localStorage[tabId.toString()+"ccMessageFL"] = ccMessageFL
-            });
-        }
-        if (localStorage.getItem(tabId.toString()+"ccMessageSL") != ccMessageSL){
-            chrome.tabs.sendMessage(parseInt(tabId),{action:"UpdateDiv",id:"ccViewSL",ccMessage:ccMessageSL}, function(res) {
-                localStorage[tabId.toString()+"ccMessageSL"] = ccMessageSL
-            });
-        }
-      });
-
+function backgroundWoker(listenTabId, tabId) {
+  chrome.tabs.sendMessage(
+    parseInt(listenTabId),
+    { action: "getLyrics" },
+    function(response) {
+      var ccMessageFL = response.ccMessage.dataFL;
+      var ccMessageSL = response.ccMessage.dataSL;
+      if (ccMessageFL == undefined) {
+        ccMessageFL = null;
+      }
+      if (ccMessageSL == undefined) {
+        ccMessageSL = null;
+      }
+      if (
+        localStorage.getItem(tabId.toString() + "ccMessageFL") != ccMessageFL
+      ) {
+        chrome.tabs.sendMessage(
+          parseInt(tabId),
+          { action: "UpdateDiv", id: "ccViewFL", ccMessage: ccMessageFL },
+          function(res) {
+            localStorage[tabId.toString() + "ccMessageFL"] = ccMessageFL;
+          }
+        );
+      }
+      if (
+        localStorage.getItem(tabId.toString() + "ccMessageSL") != ccMessageSL
+      ) {
+        chrome.tabs.sendMessage(
+          parseInt(tabId),
+          { action: "UpdateDiv", id: "ccViewSL", ccMessage: ccMessageSL },
+          function(res) {
+            localStorage[tabId.toString() + "ccMessageSL"] = ccMessageSL;
+          }
+        );
+      }
+    }
+  );
 }
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action=="Start Timer"){
-        localStorage[request.tabId.toString()+"ccMessageFL"] = null
-        localStorage[request.tabId.toString()+"ccMessageSL"] = null
-        var timerId = setInterval(function(){ backgroundWoker(request.listenTabId,request.tabId) }, 250);
-        sendResponse({ content: "Response from Background action: "+request.action,timerId: timerId})
-      }
-    if (request.action == "Stop Timer"){
-        clearInterval(request.timerId);
-        chrome.tabs.sendMessage(parseInt(request.tabId),{action:"UpdateDiv",ccMessage:''}, function(res) { });
-        sendResponse({ content: "Response from Background action: "+request.action})
-    }
-    sendResponse({ content: "Response from Background action: "+request.action})
+chrome.runtime.onMessage.addListener(function(request, sendResponse) {
+  if (request.action == "Start Timer") {
+    localStorage[request.tabId.toString() + "ccMessageFL"] = null;
+    localStorage[request.tabId.toString() + "ccMessageSL"] = null;
+    var timerId = setInterval(function() {
+      backgroundWoker(request.listenTabId, request.tabId);
+    }, 250);
+    sendResponse({
+      content: "Response from Background action: " + request.action,
+      timerId: timerId
     });
+  }
+  if (request.action == "Stop Timer") {
+    clearInterval(request.timerId);
+    chrome.tabs.sendMessage(
+      parseInt(request.tabId),
+      { action: "UpdateDiv", ccMessage: "" },
+      function(res) {}
+    );
+    sendResponse({
+      content: "Response from Background action: " + request.action
+    });
+  }
+  sendResponse({
+    content: "Response from Background action: " + request.action
+  });
+});
--- js/popup.js
+++ js/popup.js
@@ -1,90 +1,108 @@
-function listenCC(){
-  chrome.tabs.query({
-    active: true,
-    currentWindow: true
-    }, function(tabs) {
-    var listentabURL = tabs[0].url;
-    var listenTabId = tabs[0].id;
-    if (listentabURL.match("https://www.youtube.com/*") != null){
-      localStorage['listenTabId']=listenTabId;
-      localStorage['listentabURL']=listentabURL;
-      localStorage['created']=1;
-      alert("Success!");
-    }else{
-      alert("This is not youtube page you can't use this chrome extension to liseten cc lyrics in other website!")
+function listenCC() {
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true
+    },
+    function(tabs) {
+      var listentabURL = tabs[0].url;
+      var listenTabId = tabs[0].id;
+      if (listentabURL.match("https://www.youtube.com/*") != null) {
+        localStorage["listenTabId"] = listenTabId;
+        localStorage["listentabURL"] = listentabURL;
+        alert("Success! You can start to other page. Enjoy it !");
+        $("#Status").text("Listening");
+        localStoragep["status"] = 1;
+      } else {
+        alert(
+          "This is not youtube page you can't use this chrome extension to liseten cc lyrics in other website!"
+        );
+        $("#Status").text("Listen Fail");
+      }
     }
-    });
+  );
 }
 
-function showCC(){
-  if (localStorage.getItem("listenTabId")!=null){
-    $("#disappear").prop('disabled', false);
-    $("#appear").prop('disabled', true);
-    chrome.tabs.query({
-    active: true,
-    currentWindow: true
-    }, function(tabs) {
-      var tabId = tabs[0].id;
-      localStorage['tabId']=tabId;
-      StartListenCC();
-    });
+function showCC() {
+  if (localStorage.getItem("listenTabId") != null) {
+    if (localStoragep.getItem("status") != 1) {
+      localStorage["status"] = 1;
+      $("#disappear").prop("disabled", false);
+      $("#appear").prop("disabled", true);
+    }
+  } else {
+    alert("Please listen a youtube page first. Thanks");
   }
-  else{
-    alert("Please listen a youtube page first. Thanks")
-  }
-
 }
 
-function StartListenCC(){
+function StartListenCC() {
   var listenTabId = parseInt(localStorage.getItem("listenTabId"));
   var tabId = parseInt(localStorage.getItem("tabId"));
   console.log("I will send a request to background");
-  chrome.runtime.sendMessage({"action":"Start Timer","listenTabId":listenTabId,"tabId":tabId},
+  chrome.runtime.sendMessage(
+    { action: "Start Timer", listenTabId: listenTabId, tabId: tabId },
     function(response) {
       console.log("I got Start timer from background work");
-      localStorage[tabId.toString()]=response.timerId;
-      localStorage.getItem(tabId.toString())
-    });
-}
-
-function disablelistenCC(){
-  $("#appear").prop('disabled', false);
-  $("#disappear").prop('disabled', true);
-  chrome.tabs.query({
-  active: true,
-  currentWindow: true
-  }, function(tabs) {
-    var tabId = tabs[0].id;
-    chrome.runtime.sendMessage({"action":"Stop Timer","tabId":tabId,"timerId":localStorage.getItem(tabId.toString())},
-    function(response) {
-      localStorage[tabId.toString()]=null;
-      chrome.tabs.sendMessage(parseInt(tabId),{action:"UpdateDiv",id:"ccViewFL",ccMessage:null}, function(res) {
-          });
-      chrome.tabs.sendMessage(parseInt(tabId),{action:"UpdateDiv",id:"ccViewSL",ccMessage:null}, function(res) {
-          });
-    });
-  });
-}
-
-
-
-document.addEventListener('DOMContentLoaded', function(dcle) {
-  $("#listen").on('click', listenCC);
-  $("#appear").on('click', showCC);
-  $("#disappear").on('click', disablelistenCC);
-  chrome.tabs.query({
-  active: true,
-  currentWindow: true
-  }, function(tabs) {
-    var tabId = tabs[0].id;
-    if (localStorage.getItem(tabId.toString())!=null){
-      console.log("havelocalstorage");
-      $("#appear").prop('disabled', true);
-      $("#disappear").prop('disabled', false);
-    }else{
-      console.log("don'thavelocalstorage");
-      $("#appear").prop("disabled",false);
-      $("#disappear").prop('disabled', true);
+      localStorage[tabId.toString()] = response.timerId;
+      localStorage.getItem(tabId.toString());
     }
-  });
+  );
+}
+
+function disablelistenCC() {
+  $("#appear").prop("disabled", false);
+  $("#disappear").prop("disabled", true);
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true
+    },
+    function(tabs) {
+      var tabId = tabs[0].id;
+      chrome.runtime.sendMessage(
+        {
+          action: "Stop Timer",
+          tabId: tabId,
+          timerId: localStorage.getItem(tabId.toString())
+        },
+        function(response) {
+          localStorage[tabId.toString()] = null;
+          chrome.tabs.sendMessage(
+            parseInt(tabId),
+            { action: "UpdateDiv", id: "ccViewFL", ccMessage: null },
+            function(res) {}
+          );
+          chrome.tabs.sendMessage(
+            parseInt(tabId),
+            { action: "UpdateDiv", id: "ccViewSL", ccMessage: null },
+            function(res) {}
+          );
+        }
+      );
+    }
+  );
+}
+
+document.addEventListener("DOMContentLoaded", function(dcle) {
+  $("#listen").on("click", listenCC);
+  $("#appear").on("click", showCC);
+  $("#disappear").on("click", disablelistenCC);
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true
+    },
+    function(tabs) {
+      var tabId = tabs[0].id;
+      if (localStorage.getItem(tabId.toString()) != null) {
+        console.log("havelocalstorage");
+        $("#appear").prop("disabled", true);
+        $("#disappear").prop("disabled", false);
+      } else {
+        console.log("don'thavelocalstorage");
+        $("#appear").prop("disabled", false);
+        $("#disappear").prop("disabled", true);
+      }
+    }
+  );
 });
